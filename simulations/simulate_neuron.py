c441bc0cd7bc469b545ea421d30bee7e99711829
goolux <30436343+goolux@users.noreply.github.com>
2022-12-20 16:55:15 +0200

    add args.weight_scale_factor for model scaling

--- simulations/simulate_neuron.py
+++ simulations/simulate_neuron.py
@@ -1173,6 +1173,8 @@ if __name__ == "__main__":
             ID=i
             cur_input_file=os.path.join(input_path,ID)
             ID_name = f'{ID}_{sim_name}'
+            print(ID_name)
+            continue
             cur_args = args.replace(args_v.simulation_folder, os.path.join(args_v.simulation_folder, ID_name))
             cur_args = cur_args.replace(args_v.input_file, os.path.join(args_v.input_file,ID))
             s.send_job(f"simulation_{ID_name}",f"python3 -c 'from simulations.simulate_neuron import main; main()' {cur_args}")
