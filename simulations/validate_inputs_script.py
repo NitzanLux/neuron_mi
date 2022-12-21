550a32e0de89bc4acc694413aa25ccec8726f680
goolux <30436343+goolux@users.noreply.github.com>
2022-12-21 14:23:47 +0200

    add args.weight_scale_factor for model scaling

--- simulations/validate_inputs_script.py
+++ simulations/validate_inputs_script.py
@@ -16,11 +16,12 @@ for d_dir in os.listdir(base_path):
             for i in to_com:
                 ID= m_id.match(f_dir).group(0)
                 a = sparse.load_npz(os.path.join(f_dir_path,i)).toarray()
-                print(type(a))
                 a[a!=0]=1
                 b = sparse.load_npz(os.path.join('data','inputs',ID,i)).toarray()
                 b[b!=0]=1
-                if np.all(a==b):
+                print(a.shape,b.shape)
+
+                if np.array_equal(a,b):
                     print('correct')
                     continue
                 print(f_dir,d_dir)
