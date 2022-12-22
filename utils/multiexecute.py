0d96aea1984222a7e1b53b52885673f6b02457bb
goolux <30436343+goolux@users.noreply.github.com>
2022-12-22 13:01:20 +0200

    add args.weight_scale_factor for model scaling

--- utils/multiexecute.py
+++ utils/multiexecute.py
@@ -4,11 +4,11 @@ from art import tprint
 
 params = ' '.join(sys.argv[1:])
 lines = params.split('\n')
-print(lines)
+# print(lines)
 for i in lines:
-    # process = Popen('ls -la', shell=True)
-    # stdout, stderr = process.communicate()
-    # print(stdout,file=sys.stdout)
-    # print(stderr,file=sys.stderr)
+    process = Popen('ls -la', shell=True)
+    stdout, stderr = process.communicate()
+    print(stdout,file=sys.stdout)
+    print(stderr,file=sys.stderr)
     print(i)
     tprint(f'Finished')
