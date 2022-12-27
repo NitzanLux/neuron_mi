cd362bf8eb5058a0c5dd6f9210add19de60b2aa2
goolux <30436343+goolux@users.noreply.github.com>
2022-12-27 14:21:13 +0200

    add args.weight_scale_factor for model scaling

--- entropy/DSampEN.py
+++ entropy/DSampEN.py
@@ -4,7 +4,9 @@ from tqdm import tqdm
 
 NATURAL = np.exp(1)
 BASE_2 = 2
-def DSampEn(Sig, m=2, Logx=BASE_2):
+def DSampEn(Sig, m=2, Logx=NATURAL):
+
+
     """SampEn  estimates the sample entropy of a univariate data sequence.
 
     .. code-block:: python
@@ -40,22 +42,43 @@ def DSampEn(Sig, m=2, Logx=BASE_2):
     assert isinstance(m, int) and (m > 0), "m:     must be an integer > 0"
     assert isinstance(Logx, (int, float)) and (Logx > 0), "Logx:     must be a positive value"
     A = np.zeros((m+1,))
-    for k in tqdm(range(1, m + 2)):
-        A[k-1] = count_occurence(Sig,k)
+    B = np.zeros((m+1,))
+    for k in tqdm(range(1,m+2)):
+        a,b = count_occurence(Sig,k)
+        A[k-1]=a
+        B[k-1]=b
     with np.errstate(divide='ignore', invalid='ignore'):
-        Samp = -np.log(A[1:] / A[:-1] ) / np.log(Logx)
-    return Samp, A
+        Samp = -np.log(A/B)/np.log(Logx)
+    return Samp, A,B
 
 def count_occurence(sig,length):
-    repeat_dict={}
+    assert length>0,'length must be positive'
+    if length ==1:
+        cumulative_sum_B = (sig.shape[0] * (sig.shape[0]-1)/2)
+        cumulative_sum_A = np.sum([i*(i-1) for i in np.unique(sig,return_counts=True)[1]])/2
+        return cumulative_sum_A,cumulative_sum_B
+    repeat_dict_A={}
+    repeat_dict_B={}
     for i in range(sig.shape[0]-length+1):
-        cur_sig = tuple(sig[i:i+length])
-        if cur_sig not in repeat_dict:
-            repeat_dict[cur_sig]=1
+        cur_sig_A = tuple(sig[i:i+length])
+        cur_sig_B = tuple(sig[i:i+length-1])
+
+        if cur_sig_B not in repeat_dict_B:
+            repeat_dict_B[cur_sig_B]=1
         else:
-            repeat_dict[cur_sig]+=1
-    cumulative_sum=0
-    for k,v in repeat_dict.items():
-        assert v>0,f'pattern {k} appears zero times....'
-        cumulative_sum+=(v*(v-1))/2
-    return cumulative_sum
+            repeat_dict_B[cur_sig_B]+=1
+        if cur_sig_A not in repeat_dict_A:
+            repeat_dict_A[cur_sig_A]=1
+        else:
+            repeat_dict_A[cur_sig_A]+=1
+
+    cumulative_sum_B = 0
+    cumulative_sum_A = 0
+    for k,v in repeat_dict_B.items():
+        # assert v>0,f'pattern {k} appears zero times....'
+        cumulative_sum_B += (v)*(v-1)/2
+    for k,v in repeat_dict_A.items():
+        # assert v>0,f'pattern {k} appears zero times....'
+        cumulative_sum_A += (v)*(v-1)/2
+    return cumulative_sum_A ,cumulative_sum_B
+    # return cumulative_sum_B
