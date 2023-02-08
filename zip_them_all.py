4c49c175bb9e11810ca316076d4e87202c31955d
goolux <30436343+goolux@users.noreply.github.com>
2023-02-08 12:50:10 +0200

    add_zip_them_all

--- zip_them_all.py
+++ zip_them_all.py
@@ -1,21 +1,6 @@
 import argparse
-import gzip
-import multiprocessing
-import ntpath
-import re
-import traceback
-from enum import Enum
-from multiprocessing import Process, Queue
-import EntropyHub as EH
-from EntropyHub import SampEn
-import numpy as np
-import pickle as pickle
-import entropy.DSampEN as DSEN
-import entropy as ent
-from typing import List, Dict
-from utils.parse_file import parse_sim_experiment_file
-from tqdm import tqdm
 import os
+import re
 import subprocess
 
 parser = argparse.ArgumentParser(description='Regex expression for zipping different files')
@@ -28,7 +13,7 @@ args = parser.parse_args()
 m = re.compile(args.regex_expression)
 files=[]
 for i in os.listdir(args.dir):
-    if m.match(i):
+    if m.match(i) and os.path.isdir(os.path.join(args.dir,i)):
         files.append(i)
 
 cwd = os.getcwd()
