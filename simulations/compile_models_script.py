d04a0824ebcfcb4fe5c6d956bd632ebc506a0b56
goolux <30436343+goolux@users.noreply.github.com>
2023-02-13 14:23:41 +0200

    add_zip_them_all

--- simulations/compile_models_script.py
+++ simulations/compile_models_script.py
@@ -1,13 +1,19 @@
+import logging
 import os
 import subprocess
 import sys
-
 base_path= os.getcwd()
 cur_path = os.path.join(base_path,'neuron_models')
 for i in os.listdir(cur_path):
     # try:
     cwd_path=os.path.join('~',cur_path,i)
-    command = "nrnivmodl "+os.path.join('.','mechanisms')
+    if os.path.exists(os.path.join('.','mechanisms')):
+        command = "nrnivmodl "+os.path.join('.','mechanisms')
+    elif os.path.exists(os.path.join('.','mods')):
+        command = "nrnivmodl " + os.path.join('.', 'mods')
+    else:
+        logging.error(f'cannot find mod/mechanisms directory for {i}')
+        continue
     print(''.join(command),flush=True)
     if os.path.isfile(cwd_path):
         continue
