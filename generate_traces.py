005f9f33a676920bbc5e0651d36e07e5db60df46
goolux <30436343+goolux@users.noreply.github.com>
2023-02-22 16:48:24 +0200

    add file that do not exists flag to create_entropy_estimation.py

---
+++ generate_traces.py
@@ -0,0 +1,50 @@
+import argparse
+import os
+import re
+import subprocess
+import matplotlib.pyplot as plt
+import numpy as np
+from utils.parse_file import parse_sim_experiment_file
+parser = argparse.ArgumentParser(description='Regex expression for zipping different files')
+parser.add_argument('-r', dest="regex_expression", type=str,
+                    help='Regex expression for zipping files.',required=True)
+parser.add_argument('-d', dest="dir", type=str,
+                    help='Destination path', default='entropy_data')
+parser.add_argument('-f', dest="index", type=int,
+                    help='file_index',default=0)
+parser.add_argument('-i', dest="interval_length", type=int,
+                    help='interval length',default=1000)
+parser.add_argument('-s', dest="start_time", type=int,
+                    help='starting time',default=1000)
+args = parser.parse_args()
+
+m = re.compile(args.regex_expression)
+files=[]
+for i in os.listdir(args.dir):
+    if m.match(i) and os.path.isdir(os.path.join(args.dir,i)):
+        files.append(i)
+
+data_set=None
+# f_dict=dict()
+for f in files:
+    if data_set is None:
+        data_set=set( os.listdir(os.path.join(args.dir,f)))
+    else:
+        data_set = data_set.intersection(set( os.listdir(os.path.join(args.dir,f))))
+assert len(data_set)>0,"joint files were not found"
+cur_sim=list(data_set)[args.file_index]
+
+cwd = os.getcwd()
+cur_working_dir=os.path.join(cwd,args.dir)
+# os.chdir(os.path.join(cwd,args.dir))
+for f in files:
+    f_path = os.path.join(cur_working_dir,f,cur_sim)
+    _, y_spike, y_soma = parse_sim_experiment_file(f_path)
+    x = np.where(y_spike==1)[0]
+    plt.plot(y_soma[args.start_time:args.start_time+args.interval_length])
+    plt.scatter(x,y_soma[args.start_time:args.start_time+args.interval_length][x],colort = 'red')
+    cur_dir=f'{cur_sim}_{args.start_time}_{args.interval_length}'
+    os.makedirs(cur_dir,exist_ok=True)
+    plt.savefig(os.path.join(cur_dir,f'{f}.png'))
+    # p = subprocess.Popen(f"zip -r {i}.zip {i}", stdout=subprocess.PIPE, shell=True)
+    # print(p.communicate())
