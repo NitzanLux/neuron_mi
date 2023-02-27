67d92df0dd87d85d1256d439ddd662825c43270b
goolux <30436343+goolux@users.noreply.github.com>
2023-02-27 20:11:47 +0200

    add file that do not exists flag to create_entropy_estimation.py

--- update_names_of_current_no_nmda.py
+++ update_names_of_current_no_nmda.py
@@ -5,10 +5,11 @@ cur_path = os.path.join('simulations','data')
 def rename(cur_path):
     for i in os.listdir(cur_path):
         m=compile_pattern.match(i)
-        print(i)
+        # print(i)
         if m:
             print(m)
             os.rename(os.path.join(cur_path,i),os.path.join(cur_path,i.replace(m.group(1),'_noNMDA_'+m.group(2))))
             if os.path.isdir(os.path.join(cur_path,i)):
                 rename(os.path.join(cur_path,i))
 
+rename(cur_path)
