7afe152a2a1c536707d6a93125e3e3256f93358b
goolux <30436343+goolux@users.noreply.github.com>
2023-03-14 20:00:50 +0200

    add rate histogram data to all of the simulation models

--- entropy/CTW/CTW.py
+++ entropy/CTW/CTW.py
@@ -6,14 +6,19 @@ import numpy as np
 class CTW(EntropyHandler):
     def __init__(self, D: [int, None]=None,__model=None):
         self.model = (CTWManagerInfinite(__model) if D is None else CTWManagerFinite(D, True)) if __model is None else __model
-
+        self.is_constant=False
     def insert_pattern(self, p):
         print('inserting patterns',flush=True)
         if isinstance(p,np.ndarray):
             p = p.astype(int).tolist()
+        if sum(p)==0:
+            self.is_constant=True
+            return
         return self.model.update_by_sequence(p)
 
     def get_entropy(self,l,last_key:int=0):
+        if self.is_constant:
+            return 0.
         return self.model.get_entropy(l,last_key)
 
     def print_v_tree(self):
