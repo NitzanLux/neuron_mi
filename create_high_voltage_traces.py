76c9e57d8dec03e6eda1fe6aa1d96f51feea019d
goolux <30436343+goolux@users.noreply.github.com>
2023-03-14 16:57:00 +0200

    add rate histogram data to all of the simulation models

--- create_high_voltage_traces.py
+++ create_high_voltage_traces.py
@@ -15,7 +15,7 @@ def high_res_for_model_creator(model_name, input_file_name, destination_path='')
     if m:
         args = m.group(1)
         args = args.replace('Namespace','dict')
-        command=f"args = {args} ;print('sklaklsdkld')"
+        command=f"args = {args} ;print('sklaklsdkld',flush=True)"
         exec(command,globals(),locals())
         print(type(args),command)
     else:
