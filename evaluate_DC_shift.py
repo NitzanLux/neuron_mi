b7265d10c72da7bce67160fd522c4ad98d32bbee
goolux <30436343+goolux@users.noreply.github.com>
2023-03-19 17:15:43 +0200

    starting in evaluation of dc shift

--- evaluate_DC_shift.py
+++ evaluate_DC_shift.py
@@ -1,13 +1,21 @@
 import os
 from scipy import sparse
-
-length=6000
-src=os.path.join('simulations','data','inputs')
-dest=  os.path.join('simulations','data','short_inputs')
-os.makedirs(os.path.join('simulations','data','short_inputs'),exist_ok=True)
-for i in os.listdir(src):
-    os.makedirs(os.path.join(dest,i),exist_ok=True)
-    exc_weighted_spikes = sparse.load_npz(f'{os.path.join(src,i)}/exc_weighted_spikes.npz').A
-    sparse.save_npz(f'{os.path.join(dest,i)}/exc_weighted_spikes.npz', sparse.csr_matrix(exc_weighted_spikes[:,:length]))
-    inh_weighted_spikes = sparse.load_npz(f'{os.path.join(src,i)}/inh_weighted_spikes.npz').A
-    sparse.save_npz(f'{os.path.join(dest,i)}/inh_weighted_spikes.npz', sparse.csr_matrix(inh_weighted_spikes[:,:length]))
+from utils.slurm_job import SlurmJobFactory
+def create_short_inputs(length=6000):
+    src=os.path.join('simulations','data','inputs')
+    dest=  os.path.join('simulations','data','short_inputs')
+    os.makedirs(os.path.join('simulations','data','short_inputs'),exist_ok=True)
+    for i in os.listdir(src):
+        os.makedirs(os.path.join(dest,i),exist_ok=True)
+        exc_weighted_spikes = sparse.load_npz(f'{os.path.join(src,i)}/exc_weighted_spikes.npz').A
+        sparse.save_npz(f'{os.path.join(dest,i)}/exc_weighted_spikes.npz', sparse.csr_matrix(exc_weighted_spikes[:,:length]))
+        inh_weighted_spikes = sparse.load_npz(f'{os.path.join(src,i)}/inh_weighted_spikes.npz').A
+        sparse.save_npz(f'{os.path.join(dest,i)}/inh_weighted_spikes.npz', sparse.csr_matrix(inh_weighted_spikes[:,:length]))
+def create_short_simulations(models,factors, dc_range):
+    s=SlurmJobFactory(os.path.join('cluster_logs','simulation_logs'))
+    assert len(factors)==len(models)
+    for dc in dc_range:
+        for mc,f in zip(models,factors):
+            commend = f"python3 -m simulations.simulate_neuron --neuron_model_folder simulations/neuron_models/{mc} --simulation_folder /ems/elsc-labs/segev-i/nitzan.luxembourg/projects/neuron_mi/neuron_mi/simulations/short_data/{mc}_factor_{f}_DC_{dc}  --DC_shift -{dc}  --weight_scale_factor {f} --save_plots True --simulation_duration_in_seconds 6 --input_dir '/ems/elsc-labs/segev-i/nitzan.luxembourg/projects/neuron_mi/neuron_mi/simulations/short_data/short_inputs/'"
+            s.send_job(f'simulation_{mc}_{dc}_{f}',commend)
+create_short_simulations(['Rat_L5b_PC_2_Hay','Rat_L5b_PC_2_Hay_noNMDA'],[0.4,1],list(range(-90,-20,5)))
