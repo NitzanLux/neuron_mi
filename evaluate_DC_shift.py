b186ad9575d1def066d3801ae29424a56ab6799f
goolux <30436343+goolux@users.noreply.github.com>
2023-03-19 17:18:10 +0200

    starting in evaluation of dc shift

--- evaluate_DC_shift.py
+++ evaluate_DC_shift.py
@@ -1,6 +1,9 @@
 import os
 from scipy import sparse
 from utils.slurm_job import SlurmJobFactory
+from subprocess import Popen
+import sys
+
 def create_short_inputs(length=6000):
     src=os.path.join('simulations','data','inputs')
     dest=  os.path.join('simulations','data','short_inputs')
@@ -17,5 +20,10 @@ def create_short_simulations(models,factors, dc_range):
     for dc in dc_range:
         for mc,f in zip(models,factors):
             commend = f"python3 -m simulations.simulate_neuron --neuron_model_folder simulations/neuron_models/{mc} --simulation_folder /ems/elsc-labs/segev-i/nitzan.luxembourg/projects/neuron_mi/neuron_mi/simulations/short_data/{mc}_factor_{f}_DC_{dc}  --DC_shift -{dc}  --weight_scale_factor {f} --save_plots True --simulation_duration_in_seconds 6 --input_dir '/ems/elsc-labs/segev-i/nitzan.luxembourg/projects/neuron_mi/neuron_mi/simulations/short_data/short_inputs/'"
-            s.send_job(f'simulation_{mc}_{dc}_{f}',commend)
+            print(commend)
+            # s.send_job(f'simulation_{mc}_{dc}_{f}',commend)
+            process = Popen(commend, shell=True)
+            stdout, stderr = process.communicate()
+            print(stdout, file=sys.stdout)
+            print(stderr, file=sys.stderr)
 create_short_simulations(['Rat_L5b_PC_2_Hay','Rat_L5b_PC_2_Hay_noNMDA'],[0.4,1],list(range(-90,-20,5)))
