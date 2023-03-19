e9309c67fa7511a58295181ce428c520f66faf25
goolux <30436343+goolux@users.noreply.github.com>
2023-03-19 16:20:17 +0200

    starting in evaluation of dc shift

--- evaluate_DC_shift.py
+++ evaluate_DC_shift.py
@@ -6,7 +6,7 @@ src=os.path.join('simulations','data','inputs')
 dest=  os.path.join('simulations','data','short_inputs')
 os.makedirs(os.path.join('simulations','data','short_inputs'))
 for i in os.listdir(src):
-    os.makedirs(os.path.join(dest,i))
+    os.makedirs(os.path.join(dest,i),exist_ok=True)
     exc_weighted_spikes = sparse.load_npz(f'{os.path.join(src,i)}/exc_weighted_spikes.npz').A
     sparse.save_npz(f'{os.path.join(dest,i)}/exc_weighted_spikes.npz', sparse.csr_matrix(exc_weighted_spikes[:,:length]))
     inh_weighted_spikes = sparse.load_npz(f'{os.path.join(src,i)}/inh_weighted_spikes.npz').A
