52391decf789c5dc7336baaf8a342b14ac3093fe
goolux <30436343+goolux@users.noreply.github.com>
2023-03-21 15:16:17 +0200

    update summery_creator_data_from_simulations.py

--- evaluate_DC_shift.py
+++ evaluate_DC_shift.py
@@ -13,13 +13,13 @@ def create_short_inputs(length=6000):
         sparse.save_npz(f'{os.path.join(dest,i)}/exc_weighted_spikes.npz', sparse.csr_matrix(exc_weighted_spikes[:,:length]))
         inh_weighted_spikes = sparse.load_npz(f'{os.path.join(src,i)}/inh_weighted_spikes.npz').A
         sparse.save_npz(f'{os.path.join(dest,i)}/inh_weighted_spikes.npz', sparse.csr_matrix(inh_weighted_spikes[:,:length]))
-def create_short_simulations(models,factors, dc_range):
+def create_short_simulations(models,factors, dc_range,data_folder='data',input_dir='inputs'):
     assert len(factors)==len(models)
     for dc in dc_range:
         for mc,f in zip(models,factors):
             # dc=abs(dc)
             f_str=str(f).replace('.','-')
-            commend = f"python3 -m simulations.simulate_neuron --neuron_model_folder simulations/neuron_models/{mc} --simulation_folder /ems/elsc-labs/segev-i/nitzan.luxembourg/projects/neuron_mi/neuron_mi/simulations/data/{mc}_factor_{f_str}_DC_{dc}  --DC_shift {dc}  --weight_scale_factor {f} --save_plots True --simulation_duration_in_seconds 60 --input_dir '/ems/elsc-labs/segev-i/nitzan.luxembourg/projects/neuron_mi/neuron_mi/simulations/data/inputs/'"
+            commend = f"python3 -m simulations.simulate_neuron --neuron_model_folder simulations/neuron_models/{mc} --simulation_folder /ems/elsc-labs/segev-i/nitzan.luxembourg/projects/neuron_mi/neuron_mi/simulations/{data_folder}/{mc}_factor_{f_str}_DC_{dc}  --DC_shift {dc}  --weight_scale_factor {f} --save_plots True --simulation_duration_in_seconds 60 --input_dir '/ems/elsc-labs/segev-i/nitzan.luxembourg/projects/neuron_mi/neuron_mi/simulations/{data_folder}/{input_dir}/'"
             print(commend)
             # s.send_job(f'simulation_{mc}_{dc}_{f}',commend)
             process = Popen(commend, shell=True)
@@ -39,4 +39,4 @@ def create_entropy_approximation(models,factors, dc_range):
             stdout, stderr = process.communicate()
             print(stdout, file=sys.stdout)
             print(stderr, file=sys.stderr)
-create_entropy_approximation(['Rat_L5b_PC_2_Hay','Rat_L5b_PC_2_Hay_noNMDA'],[0.2,1],list(range(-90,-30,10)))
+create_short_simulations(['Rat_L5b_PC_2_Hay','Rat_L5b_PC_2_Hay_noNMDA'],[0.1,1],list(range(-90,-30,10)),data_folder='short_data')
