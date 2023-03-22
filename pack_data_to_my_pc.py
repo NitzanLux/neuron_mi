9138122823243bd468e5ecfe2af4b07aea616913
goolux <30436343+goolux@users.noreply.github.com>
2023-03-22 19:43:34 +0200

    update summery_creator_data_from_simulations.py

--- pack_data_to_my_pc.py
+++ pack_data_to_my_pc.py
@@ -6,7 +6,7 @@ dest_path='slim_data'
 os.makedirs(dest_path)
 for m in os.listdir(src_path):
     temp_path = os.path.join(src_path, m)
-    for sim in os.path.join(temp_path):
+    for sim in os.listdir(os.path.join(temp_path)):
         os.makedirs(os.path.join(dest_path,sim),exist_ok=True)
         os.makedirs(os.path.join(dest_path, sim,m), exist_ok=True)
         for f in os.listdir(os.path.join(temp_path,sim)):
