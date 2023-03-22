e12fe3d439935dd7a523315121a03ad473260bff
goolux <30436343+goolux@users.noreply.github.com>
2023-03-22 19:42:27 +0200

    update summery_creator_data_from_simulations.py

--- pack_data_to_my_pc.py
+++ pack_data_to_my_pc.py
@@ -11,6 +11,6 @@ for m in os.listdir(src_path):
         os.makedirs(os.path.join(dest_path, sim,m), exist_ok=True)
         for f in os.listdir(os.path.join(temp_path,sim)):
             if '.h5' in f:
-                shutil.move(os.path.join(src_path,m,sim,f),os.path.join(dest_path,sim,m,f))
+                shutil.copyfile(os.path.join(src_path,m,sim,f),os.path.join(dest_path,sim,m,f))
 shutil.make_archive(f'{dest_path}.zip', 'zip', dest_path)
 shutil.rmtree(dest_path)
