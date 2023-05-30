20d27c4e98c1736615ae8b7b067cc69460f86750
goolux <30436343+goolux@users.noreply.github.com>
2023-05-30 15:28:19 +0300

    cheack unbounded probabilities

--- simulations/evaluate_isi.py
+++ simulations/evaluate_isi.py
@@ -7,10 +7,10 @@ from utils.parse_file import parse_sim_experiment_file
 data = dict()
 # for r,d,_ in os.walk(os.path.join("simulation","data")):
 # print([(i,p,j) for i,p,j in os.walk(os.path.join("simulations","data"))])
-for r,_,_ in os.walk(os.path.join("simulations","data")):
+base_path = os.path.join("simulations","data")
+for r,_,_ in os.walk(base_path):
     data[r]=dict()
     print(r)
-
     for f in r:
         _, s, v = parse_sim_experiment_file(f)
         # print(s)
