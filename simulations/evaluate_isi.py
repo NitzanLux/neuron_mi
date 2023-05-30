be4e3a7a142a7fd1d8c153387574f2d7985d4fe9
goolux <30436343+goolux@users.noreply.github.com>
2023-05-30 15:27:07 +0300

    cheack unbounded probabilities

--- simulations/evaluate_isi.py
+++ simulations/evaluate_isi.py
@@ -6,14 +6,14 @@ import pickle
 from utils.parse_file import parse_sim_experiment_file
 data = dict()
 # for r,d,_ in os.walk(os.path.join("simulation","data")):
-print([(i,p,j) for i,p,j in os.walk(os.path.join("simulation","data"))])
-for r,p,_ in os.walk(os.path.join("simulation","data")):
+# print([(i,p,j) for i,p,j in os.walk(os.path.join("simulations","data"))])
+for r,_,_ in os.walk(os.path.join("simulation","data")):
     data[r]=dict()
-    print(p)
+    print(r)
 
     for f in r:
         _, s, v = parse_sim_experiment_file(f)
-        print(s)
+        # print(s)
         data[r][f] = s
 with open('spikes_datar.pkl','wb') as f:
     pickle.dump(data,f)
