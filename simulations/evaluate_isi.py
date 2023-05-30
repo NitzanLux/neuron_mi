899b6b8992afe9a661ad200865bf67c38f963f16
goolux <30436343+goolux@users.noreply.github.com>
2023-05-30 15:40:09 +0300

    cheack unbounded probabilities

--- simulations/evaluate_isi.py
+++ simulations/evaluate_isi.py
@@ -3,6 +3,7 @@ import pandas as pd
 import os
 import pickle
 # from .. import utils as u
+from tqdm import tqdm
 from utils.parse_file import parse_sim_experiment_file
 data = dict()
 # for r,d,_ in os.walk(os.path.join("simulation","data")):
@@ -10,11 +11,11 @@ data = dict()
 base_path = os.path.join("simulations","data")
 for _,d,_ in os.walk(base_path):
     print(d)
-    for d_m in d:
+    for d_m in tqdm(d):
         data[d_m] = dict()
 
         for r_d,d_f,_ in os.walk(os.path.join(base_path,d_m)):
-            for d_ff in d_f:
+            for d_ff in tqdm(d_f):
                 # for r,d_a,f in os.walk(os.path.join(base_path,d_f)):
                 try:
                     _, s, v = parse_sim_experiment_file(os.path.join(r_d,d_ff))
